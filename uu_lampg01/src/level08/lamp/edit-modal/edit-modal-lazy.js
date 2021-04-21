8675be518e737d9ef3aa0e7ac95aa79ea9d5e7b6
Martin Farkaš <martin.farkas@unicorn.com>
2021-04-21 15:52:39 +0200

    Add Level08.Lamp component

---
+++ uu_lampg01/src/level08/lamp/edit-modal/edit-modal-lazy.js
@@ -0,0 +1,143 @@
+//@@viewOn:imports
+import * as UU5 from "uu5g04";
+import { createComponentWithRef, useRef, useImperativeHandle } from "uu5g04-hooks";
+import "uu5g04-bricks";
+import Config from "./config/config";
+import Lsi from "./edit-modal-lazy-lsi";
+//@@viewOff:imports
+
+//TODO MFA - Add documentation link to info header
+
+const STATICS = {
+  //@@viewOn:statics
+  displayName: Config.TAG + "EditModalLazy",
+  //@@viewOff:statics
+};
+
+const EditModalLazy = createComponentWithRef({
+  ...STATICS,
+
+  //@@viewOn:propTypes
+  propTypes: {
+    props: UU5.PropTypes.object,
+    onClose: UU5.PropTypes.func,
+  },
+  //@@viewOff:propTypes
+
+  //@@viewOn:defaultProps
+  defaultProps: {
+    props: undefined,
+    onClose: undefined,
+  },
+  //@@viewOff:defaultProps
+
+  render({ props, onClose }, ref) {
+    //@@viewOn:private
+    const modalRef = useRef();
+    //@@viewOff:private
+
+    //@@viewOn:interface
+    useImperativeHandle(ref, () => ({
+      getPropsToSave: modalRef.current.getPropsToSave,
+    }));
+    //@@viewOff:interface
+
+    //@@viewOn:render
+    return (
+      <UU5.BricksEditable.Modal
+        header={<UU5.Bricks.Lsi lsi={{ en: "Edit Level08.Lamp" }} />}
+        shown
+        onClose={onClose}
+        componentName={"UuLamp.Level08.Lamp"}
+        componentProps={props}
+        componentPropsForm={[
+          {
+            name: <UU5.Bricks.Lsi lsi={Lsi.properties} />,
+            setup: [
+              {
+                name: "bulbStyle",
+                type: "switchSelector",
+                label: Lsi.bulbStyle,
+                getProps: () => {
+                  return {
+                    items: [
+                      { content: "filled", value: "filled" },
+                      { content: "outline", value: "outline" },
+                    ],
+                  };
+                },
+              },
+              {
+                name: "bulbSize",
+                type: "switchSelector",
+                label: Lsi.bulbSize,
+                getProps: () => {
+                  return {
+                    items: [
+                      { content: "s", value: "s" },
+                      { content: "m", value: "m" },
+                      { content: "l", value: "l" },
+                      { content: "xl", value: "xl" },
+                    ],
+                  };
+                },
+              },
+              {
+                name: "header",
+                type: "text",
+                label: Lsi.header,
+              },
+            ],
+            info: <UU5.Bricks.Lsi lsi={Lsi.info} params={[]} />,
+          },
+          {
+            name: <UU5.Bricks.Lsi lsi={Lsi.visual} />,
+            setup: [
+              {
+                name: "cardView",
+                type: "switchSelector",
+                label: Lsi.cardView,
+                getProps: () => {
+                  return {
+                    items: [
+                      { content: "none", value: "none" },
+                      { content: "full", value: "full" },
+                      { content: "content", value: "content" },
+                    ],
+                  };
+                },
+              },
+              {
+                name: "colorSchema",
+                type: "colorSchema",
+                label: Lsi.colorSchema,
+              },
+              {
+                name: "bgStyle",
+                type: "bgStyle",
+                label: Lsi.bgStyle,
+              },
+              {
+                name: "elevation",
+                type: "elevation",
+                label: Lsi.elevation,
+              },
+              {
+                name: "borderRadius",
+                type: "borderRadius",
+                label: Lsi.borderRadius,
+              },
+            ],
+            info: <UU5.Bricks.Lsi lsi={Lsi.info} />,
+          },
+        ]}
+        ref_={modalRef}
+      />
+    );
+    //@@viewOff:render
+  },
+});
+
+//viewOn:exports
+export default EditModalLazy;
+//viewOff:exports
---
+++ uu_lampg01/src/level08/lamp/lamp-core.js
@@ -0,0 +1,77 @@
+//@@viewOn:imports
+import UU5 from "uu5g04";
+import { createVisualComponent, TimeZoneProvider } from "uu5g04-hooks";
+import Core from "../../core/core";
+import Config from "./config/config";
+import LampView from "./lamp-view";
+
+//@@viewOff:imports
+
+const STATICS = {
+  //@@viewOn:statics
+  displayName: Config.TAG + "LampCore",
+  //@@viewOff:statics
+};
+
+export const LampCore = createVisualComponent({
+  //@@viewOn:statics
+  ...STATICS,
+  //@@viewOff:statics
+
+  //@@viewOn:propTypes
+  propTypes: {
+    timeZone: UU5.PropTypes.string,
+    header: UU5.PropTypes.node,
+    help: UU5.PropTypes.node,
+    on: UU5.PropTypes.bool,
+    bulbStyle: UU5.PropTypes.oneOf(["filled", "outline"]),
+    bulbSize: UU5.PropTypes.oneOf(["s", "m", "l", "xl"]),
+    bgStyle: UU5.PropTypes.string,
+    cardView: UU5.PropTypes.string,
+    colorSchema: UU5.PropTypes.string,
+    elevation: UU5.PropTypes.oneOfType([UU5.PropTypes.string, UU5.PropTypes.number]),
+    borderRadius: UU5.PropTypes.oneOfType([UU5.PropTypes.string, UU5.PropTypes.number]),
+  },
+  //@@viewOff:propTypes
+
+  //@@viewOn:defaultProps
+  defaultProps: {
+    timeZone: "Europe/Prague",
+    header: "",
+    help: "",
+    on: false,
+    bulbStyle: "filled",
+    bulbSize: "xl",
+    bgStyle: "transparent",
+    cardView: "full",
+    colorSchema: "amber",
+    elevation: 1,
+    borderRadius: "0",
+  },
+  //@@viewOff:defaultProps
+
+  render(props) {
+    //@@viewOn:render
+    const attrs = UU5.Common.VisualComponent.getAttrs(props);
+
+    return (
+      <LampView
+        header={props.header}
+        copyTagFunc={props.copyTagFunc}
+        on={true}
+        bulbStyle={props.bulbStyle}
+        bulbSize={props.bulbSize}
+        bgStyle={props.bgStyle}
+        cardView={props.cardView}
+        colorSchema={props.colorSchema}
+        elevation={props.elevation}
+        borderRadius={props.borderRadius}
+        nestingLevel={props.nestingLevel}
+        {...attrs}
+      />
+    );
+  },
+  //@@viewOff:render
+});
+
+export default Core.withAuthentication(LampCore, STATICS.displayName);
