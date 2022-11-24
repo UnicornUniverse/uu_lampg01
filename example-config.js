c4c39c38ec9659e285258727a9d577ccb213c5fc
Martin Farkaš <martin.farkas@unicorn.com>
2022-11-24 19:53:00 +0100

    Revision of  dependencies

--- example-config.js
+++ example-config.js
@@ -3,31 +3,28 @@
 
 // uses ES5 syntax
 (function () {
-  var cdnBaseUri = new URL(
-    document.currentScript.getAttribute("data-orig-src") ||
-      document.currentScript.src
-  ).origin;
+  var cdnBaseUri = new URL(document.currentScript.getAttribute("data-orig-src") || document.currentScript.src).origin;
   var imports = {}; // this line will be auto-replaced by devkit
-  for (var k in imports)
-    imports[k] = new URL(imports[k], cdnBaseUri).toString();
+  for (var k in imports) imports[k] = new URL(imports[k], cdnBaseUri).toString();
 
-  imports["uu_plus4u5g02-app"] =
-    "https://cdn.plus4u.net/uu-plus4u5g02/1.0.0/uu_plus4u5g02-app.min.js";
-  imports["uu5g05-dev"] =
-    "https://cdn.plus4u.net/uu-uu5g05/1.0.0/uu5g05-dev.min.js";
-  imports["uu5richtextg01"] =
-    "https://cdn.plus4u.net/uu-uu5richtextg01/1.0.0/uu5richtextg01.min.js";
-  imports["immutable"] =
-    "https://cdn.plus4u.net/libs/immutable/3.8.2/immutable.min.js";
-  imports["uu_editablecomponentcontentg03"] =
-    "https://cdn.plus4u.net/uu-editablecomponentcontentg03/3.0.0/uu_editablecomponentcontentg03.min.js";
-  imports["uu_applibraryregistryg01-bricks"] =
-    "https://cdn.plus4u.net/uu-applibraryregistryg01/1.0.0/uu_applibraryregistryg01-bricks.min.js";
-  imports["uu5tilesg02"] =
-    "https://cdn.plus4u.net/uu-uu5tilesg02/1.0.0/uu5tilesg02.min.js";
-  imports["uu5codekitg01"] =
-    "https://cdn.plus4u.net/uu-uu5codekitg01/1.0.0/uu5codekitg01.min.js";
+  // prettier-ignore
+  {
+  imports["create-react-class"] = "https://cdn.plus4u.net/libs/create-react-class/15.6.3/create-react-class.min.js";
+  imports["immutable"] = "https://cdn.plus4u.net/libs/immutable/3.8.2/immutable.min.js";
+  imports["uu5g04"] = "https://cdn.plus4u.net/uu-uu5g04/1.0.0/uu5g04.min.js";
+  imports["uu5g04-bricks"] = "https://cdn.plus4u.net/uu-uu5g04/1.0.0/uu5g04-bricks.min.js";
+  imports["uu5g04-forms"] = "https://cdn.plus4u.net/uu-uu5g04/1.0.0/uu5g04-forms.min.js";
+  imports["uu5g04-hooks"] = "https://cdn.plus4u.net/uu-uu5g04/1.0.0/uu5g04-hooks.min.js";
+  imports["uu5g04-block-layout"] = "https://cdn.plus4u.net/uu-uu5g04/1.0.0/uu5g04-block-layout.min.js";
+  imports["uu_plus4u5g02-app"] = "https://cdn.plus4u.net/uu-plus4u5g02/1.0.0/uu_plus4u5g02-app.min.js";
+  imports["uu5g05-dev"] = "https://cdn.plus4u.net/uu-uu5g05/1.0.0/uu5g05-dev.min.js";
+  imports["uu5richtextg01"] = "https://cdn.plus4u.net/uu-uu5richtextg01/1.0.0/uu5richtextg01.min.js";
+  imports["uu_editablecomponentcontentg03"] = "https://cdn.plus4u.net/uu-editablecomponentcontentg03/3.0.0/uu_editablecomponentcontentg03.min.js";
+  imports["uu_applibraryregistryg01-bricks"] = "https://cdn.plus4u.net/uu-applibraryregistryg01/1.0.0/uu_applibraryregistryg01-bricks.min.js";
+  imports["uu5tilesg02"] = "https://cdn.plus4u.net/uu-uu5tilesg02/1.0.0/uu5tilesg02.min.js";
+  imports["uu5codekitg01"] = "https://cdn.plus4u.net/uu-uu5codekitg01/1.0.0/uu5codekitg01.min.js";
   imports["uu_lampg01"] = "/uu_lampg01.js";
+  }
 
   window.Uu5Loader.config({ imports: imports });
 })();
